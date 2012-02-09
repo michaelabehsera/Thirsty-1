e3ef153f5da9a085bf7913b166b036c3f34c7a50
Chris Bolton <saiko_chriskun@shortmail.me>
2012-02-09 14:18:30 -0800

    month-to-month timer

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,10 +2,17 @@ class Article
   include Mongoid::Document
   include Mongoid::Timestamps::Created
 
+  before_create :add_month
+
   field :url, type: String
   field :unique_pageviews, type: Integer, default: 0
+  field :month, type: Integer
 
   has_one :notification
   belongs_to :user
   belongs_to :campaign
+
+  def add_month
+    self.month = self.campaign.month
+  end
 end
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,6 +2,7 @@ class Campaign
   include Mongoid::Document
 
   before_save :parse_url
+  before_create :time_setup
 
   field :uuid, type: String
   index :uuid, unique: true
@@ -10,6 +11,10 @@ class Campaign
   field :notes, type: String
   field :url, type: String
   field :analytics_id, type: String
+
+  field :month, type: Integer, default: 0
+  field :start_day, type: Integer
+  field :end_date, type: Date
 
   field :username, type: String
   field :pass, type: String
@@ -23,5 +28,15 @@ class Campaign
 
   def parse_url
     self.url = 'http://' + self.url if self.url[0..6] != 'http://'
+  end
+
+  def time_setup
+    self.start_day = Time.now.day
+    advance_month
+  end
+
+  def advance_month
+    self.month = self.month + 1
+    self.end_date = Time.now + 1.month
   end
 end
