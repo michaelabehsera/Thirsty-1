13eff728a3b517e1796af8cf5f25d2010f116cc4
Chris Bolton <chriskun@saikonet.org>
2012-02-11 02:11:36 -0500

    precompile fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,13 +10,12 @@ Thirsty::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
-  config.assets.precompile += %w( signup.css )
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile += %w( signup.css )
 
   # Generate digests for assets URLs
   config.assets.digest = true
